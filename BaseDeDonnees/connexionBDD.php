8d5aa4fcb2854da8656f9e6cd2288e5cf6079a7b
Lucien Leseigle <ggenesum@gmail.com>
2024-03-15 08:44:09 +0100

    RESTORE BDD CONNECTION SCRIPT change livreur.DIsponible to boolean in db creation script fix bugs for 'gestionLivreurs'

---
+++ BaseDeDonnees/connexionBDD.php
@@ -0,0 +1,9 @@
+    <?php
+    require_once 'codesConnexion.php';
+    try {
+        $conn = new PDO('mysql:host=' . HOST . ';charset=utf8;dbname=' . DATABASE.';port='.PORT, ADMIN_USER, ADMIN_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (PDOException $e) {
+        echo 'Erreur : ' . $e->getMessage() . '<br />';
+        echo 'N° : ' . $e->getCode();
+        die();
+    }
