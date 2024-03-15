149fe55b9a74411c5b0ca32966147e8481cbffcc
Lucien Leseigle <ggenesum@gmail.com>
2024-03-15 11:52:28 +0100

    Ajout données inventaire

--- BaseDeDonnees/Crepes-Create.sql
+++ BaseDeDonnees/Crepes-Create.sql
@@ -253,12 +253,11 @@ CREATE TABLE Inventaire (
     IdProd INT NOT NULL,
     Date DATE NOT NULL,
     Quantite INT NOT NULL,
+    StockTheorique INT NOT NULL,
     CONSTRAINT PK_Inventaire PRIMARY KEY (IdInventaire),
     CONSTRAINT FK_Inventaire_Produit FOREIGN KEY (IdProd)
         REFERENCES PRODUIT(IdProd)
 );
-
-
 
 /* Assurez-vous que les événements programmés sont activés dans votre serveur MySQL. 
 Vous pouvez vérifier cela avec la commande SHOW VARIABLES LIKE 'event_scheduler'; 
