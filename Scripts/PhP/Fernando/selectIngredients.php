2b663f61951660da15c9e883c89a157d54754804
Facou <facoubot2002@gmail.com>
2024-04-02 12:51:59 +0200

    Changeements

--- Scripts/PhP/Fernando/selectIngredients.php
+++ Scripts/PhP/Fernando/selectIngredients.php
@@ -1,9 +1,13 @@
+
 <?php
 //Created by Fernando FERREIRA PIAIA
 //Date : 2024-03-15
 
-require_once '..\..\..\BaseDeDonnees\codesConnexion.php';
+require_once '../../../BaseDeDonnees/codesConnexion.php';
+
 // Connexion à la base de données
+
+
 try {
     $connexionPDO = new PDO('mysql:host=' . HOST . ';charset=utf8;dbname='
         . DATABASE . ';port=' . PORT, ADMIN_USER, ADMIN_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
@@ -13,39 +17,48 @@ try {
     die();
 }
 
-$tableauIdIngredients = array();
+
+if (isset($_POST['fournisseurs'])) {
+    $fournisseurs = $_POST['fournisseurs'];
 $tableauIngredients = array();
 try {
-    $commandeSQL = "SELECT IdIngred FROM fourn_ingr WHERE NomFourn = 'Coca-Cola'";
-    $ingredients = $connexionPDO->query($commandeSQL);
-    $ingredients = $ingredients->fetchAll(PDO::FETCH_ASSOC);
+    $commandeSQL = "SELECT IdIngred FROM fourn_ingr where NomFourn = '$fournisseurs'";
+    $idIngredients = $connexionPDO->query($commandeSQL);
+    $idIngredients = $idIngredients->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($ingredients as $idIngredient) {
-        foreach ($idIngredient as $id) {
-            array_push($tableauIdIngredients, $id);
+    foreach ($idIngredients as $id) {
+        foreach ($id as $sousId) {
+            $tableauIngredients[$sousId] = null;
         }
     }
-} catch (Exception $e) {
+}catch (Exception $e) {
     echo 'Erreur : ' . $e->getMessage() . '<br />';
     echo 'N° : ' . $e->getCode();
     die();
 }
 
-try {
-    foreach ($tableauIdIngredients as $id){
-        $commandeSQL = "SELECT NomIngred FROM ingredient WHERE IdIngred =" . $id;
-        $nomIngredient = $connexionPDO->query($commandeSQL);
-        $nomIngredient = $nomIngredient->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($nomIngredient as $nom){
-            $tableauIngredients[$id] = $nom['NomIngred'];
+foreach ($tableauIngredients as $id => $ingredient) {
+    try {
+        $commandeSQL = "SELECT NomIngred FROM ingredient where IdIngred = $id";
+        $ingred = $connexionPDO->query($commandeSQL);
+        $ingred = $ingred->fetchAll(PDO::FETCH_ASSOC);
+    
+        foreach ($ingred as $ingredien) {
+            foreach ($ingredien as $sousingredien) {
+                $tableauIngredients[$id] = $sousingredien;
+            }
         }
+    }catch (Exception $e) {
+        echo 'Erreur : ' . $e->getMessage() . '<br />';
+        echo 'N° : ' . $e->getCode();
+        die();
     }
-} catch (Exception $e) {
-    echo 'Erreur : ' . $e->getMessage() . '<br />';
-    echo 'N° : ' . $e->getCode();
-    die();
 }
-
+    
+var_dump($tableauIngredients);
 $data = json_encode($tableauIngredients);
-$fileadress = "../../JavaScript/Fernando/ingredientsId.json";
+$fileadress = "../../../Scripts/JavaScript/Fernando/ingredientsId.json";
 file_put_contents($fileadress, $data);
+} else {
+    echo "Erreur";
+}
