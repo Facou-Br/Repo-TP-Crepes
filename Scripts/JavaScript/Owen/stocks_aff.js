c0f129ff4d9f7827fa8ac2fe1c397a2561024d47
0wen2886 <128720196+0wn2886@users.noreply.github.com>
2024-04-04 19:30:45 +0200

    oo

--- Scripts/JavaScript/Owen/stocks_aff.js
+++ Scripts/JavaScript/Owen/stocks_aff.js
@@ -2,21 +2,25 @@ $(".aff_stock").change(function(){
     $("div").empty();
     $.ajax({
         url: "../../PhP/Owen/stock/afficher_stock.php",
-        type: "POST",
+        type: "GET",
         data: 'action=' + $(this).attr('value'),
         success: function (data) {
             $.getJSON("stocks.json", function(data) {
-                $.each(data, function(index, item) {
+                $.each(data, function(index, row) {
                     $(".aff_stock").append(
-                        "<td value=".$row['NomIngred'].>.$row['NomIngred'].</td>;
-                        <td value=.$row['NomFourn'].>.$row['NomFourn']."</td>;
-                        <td value='.$row['SeuilStock'].'>".$row['SeuilStock']."</td>;
-                        <td value='.$row['StockMin'].'>.$row['StockMin']."</td>;
-                        <td value='.$row['StockReel'].'>".$row['StockReel']."</td>;
-                        <td value='.$row['PrixUHT_Moyen'].'>".$row['PrixUHT_Moyen']."</td>";
-                        <td><button type='button' class='modif_stock'>'>Modifier</button></td>";)
+                        "<tr>" +
+                        "<td value='" + row['NomIngred'] + "'>" + row['NomIngred'] + "</td>" +
+                        "<td value='" + row['NomFourn'] + "'>" + row['NomFourn'] + "</td>" +
+                        "<td value='" + row['SeuilStock'] + "'>" + row['SeuilStock'] + "</td>" +
+                        "<td value='" + row['StockMin'] + "'>" + row['StockMin'] + "</td>" +
+                        "<td value='" + row['StockReel'] + "'>" + row['StockReel'] + "</td>" +
+                        "<td value='" + row['PrixUHT_Moyen'] + "'>" + row['PrixUHT_Moyen'] + "</td>" +
+                        "<td><button type='button' class='modif_stock'>Modifier</button></td>" +
+                        "</tr>"
+                    );
                 });
             });
+
         }, // closing brace and parenthesis for success function
         error: function () {
             alert("Erreur lors de la récupération des stocks.");
