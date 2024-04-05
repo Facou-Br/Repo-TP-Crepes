443f08de1787bb9a370b6cb2d6cd7642927b5b78
Facou <facoubot2002@gmail.com>
2024-04-05 09:16:31 +0200

    Mise à jour de l'affichage des stocks dans stocks_aff.js

--- Scripts/JavaScript/Owen/stocks_aff.js
+++ Scripts/JavaScript/Owen/stocks_aff.js
@@ -1,30 +1,52 @@
-$(".aff_stock").change(function(){
-    var actionValue = $(this).val();
-    $("div").empty();
-    $.ajax({
-        url: "../../PhP/Owen/stock/afficher_stock.php",
-        type: "POST",
-        data: {action: actionValue},
-        success: function (data) {
-            $.getJSON("stocks.json", function(data) {
-                $.each(data, function(index, row) {
-                    $("div").append(
-                        "<tr>" +
-                        "<td value='" + row['NomIngred'] + "'>" + row['NomIngred'] + "</td>" +
-                        "<td value='" + row['NomFourn'] + "'>" + row['NomFourn'] + "</td>" +
-                        "<td value='" + row['SeuilStock'] + "'>" + row['SeuilStock'] + "</td>" +
-                        "<td value='" + row['StockMin'] + "'>" + row['StockMin'] + "</td>" +
-                        "<td value='" + row['StockReel'] + "'>" + row['StockReel'] + "</td>" +
-                        "<td value='" + row['PrixUHT_Moyen'] + "'>" + row['PrixUHT_Moyen'] + "</td>" +
-                        "<td><button type='button' class='modif_stock'>Modifier</button></td>" +
-                        "</tr>"
-                    );
-                });
-            });
+let actionValue = $(".stocks").val();
+//$(".stocks").empty();
 
-        }, // closing brace and parenthesis for success function
-        error: function () {
-            alert("Erreur lors de la récupération des stocks.");
-        },
+$.ajax({
+  url: "../../../Scripts/PhP/Owen/stock/afficher_stock.php",
+  type: "POST",
+  success: function (data) {
+    $.getJSON("../../../Scripts/JavaScript/Owen/stocks.json", function (data) {
+        alert("Erreur lors de la récupération des stocks.");
+      $.each(data, function (index, row) {
+        $(".stocks").append(
+          "<tr>" +
+            "<td value='" +
+            row["NomIngred"] +
+            "'>" +
+            row["NomIngred"] +
+            "</td>" +
+            "<td value='" +
+            row["NomFourn"] +
+            "'>" +
+            row["NomFourn"] +
+            "</td>" +
+            "<td value='" +
+            row["SeuilStock"] +
+            "'>" +
+            row["SeuilStock"] +
+            "</td>" +
+            "<td value='" +
+            row["StockMin"] +
+            "'>" +
+            row["StockMin"] +
+            "</td>" +
+            "<td value='" +
+            row["StockReel"] +
+            "'>" +
+            row["StockReel"] +
+            "</td>" +
+            "<td value='" +
+            row["PrixUHT_Moyen"] +
+            "'>" +
+            row["PrixUHT_Moyen"] +
+            "</td>" +
+            "<td><button type='button' class='modif_stock'>Modifier</button></td>" +
+            "</tr>"
+        );
+      });
     });
+  }, // closing brace and parenthesis for success function
+  error: function () {
+    alert("Erreur lors de la récupération des stocks.");
+  },
 });
