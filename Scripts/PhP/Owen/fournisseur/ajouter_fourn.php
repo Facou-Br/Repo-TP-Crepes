09dffb6c31057431a6c6fd7f6e08a541045163bf
0wen2886 <128720196+0wn2886@users.noreply.github.com>
2024-04-23 10:14:26 +0200

    oo

--- Scripts/PhP/Owen/fournisseur/ajouter_fourn.php
+++ Scripts/PhP/Owen/fournisseur/ajouter_fourn.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../../BaseDeDonnees/codesConnexion.php';
+require_once '../../../../BaseDeDonnees/codesConnexion.php';
 try {
     $connex = new PDO('mysql:host=' . HOST . ';charset=utf8;dbname=' . DATABASE.';port='.PORT, ADMIN_USER, ADMIN_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (PDOException $e) {
@@ -7,18 +7,17 @@ try {
     echo 'N° : ' . $e->getCode();
     die();
 }
-$form= $_POST;
-$nomFourn = $form["nomFourn"];
-$adresse = $form["adresse"];
-$cp = $form["codePostal"];
-$ville = $form["ville"];
-$tel = $form["telephone"];
-var_dump($form);
+$nomFourn = $_POST["nomFourn"];
+$adresse = $_POST["adresse"];
+$cp = $_POST["codePostal"];
+$ville = $_POST["ville"];
+$tel = $_POST["telephone"];
+echo $nomFourn . " " . $adresse . " " . $cp . " " . $ville . " " . $tel;
 try {
     $connex->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
-    $sql = "INSERT INTO `fournisseur` (`NomFourn`, `Adresse`, `CodePostal`, `Ville`, `Tel`, `DateArchiv`) VALUES (?, ?, ?, ?, ?, NOW())";
-    $stmt = $connex->prepare($sql);
-    $stmt->execute([$nomFourn, $adresse, $cp, $ville, $tel]);
+    $sql = "INSERT INTO `fournisseur` (`NomFourn`, `Adresse`, `CodePostal`, `Ville`, `Tel`, `DateArchiv`) VALUES ($nomFourn, $adresse, $cp, $ville, $tel, NOW());";
+    $connex->exec($sql);
+    echo "Fournisseur ajouté";
 } catch (PDOException $e) {
     echo 'Erreur : ' . $e->getMessage() . '<br />';
     echo 'N° : ' . $e->getCode();
