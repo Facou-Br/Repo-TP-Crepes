4572b175b0ef3d892a3973ff6923422dae1c305f
0wen2886 <128720196+0wn2886@users.noreply.github.com>
2024-04-23 08:25:32 +0200

    oo

--- Scripts/JavaScript/Owen/fourn_aj.js
+++ Scripts/JavaScript/Owen/fourn_aj.js
@@ -1,37 +1,16 @@
 $(document).ready(function (){
-$("#Ajouter").click(function(){
-    $.ajax({
-        type:'POST',
-        url:'../../../../PHP/Owen/fournisseur/ajouter_fournisseur.php',
-        data: $(".form_fourn").serialize(),
-        success: function(){
-            alert("Fournisseur ajouté avec succès.");
-        },
-        error: function (){
-            alert("Erreur lors de l'envoi du formulaire.");
-        }
+    $("#form_fourn").submit(function(event){
+        event.preventDefault();
+        $.ajax({
+            type:'POST',
+            url:'../../../../PHP/Owen/fournisseur/ajouter_fournisseur.php',
+            data: $(this).serialize(),
+            success: function(){
+                alert("Fournisseur ajouté avec succès.");
+            },
+            error: function (){
+                alert("Erreur lors de l'envoi du formulaire.");
+            }
+        });
     });
-});
-});
-
-
-// Trigger an event when the form is submitted
-const form = document.getElementById('form_fourn');
-form.addEventListener('submit', (event) => {
-
-    // Get form data
-    let nameFourn = document.getElementById('nomFourn').value;
-    let adresse = document.getElementById('adresse').value;
-    let codePostal = document.getElementById('codePostal').value;
-    let ville = document.getElementById('ville').value;
-    let telephone = document.getElementById('telephone').value;
-
-    // Ajax query
-    $.ajax('../../../../HTML')
-        .post({ "nameFourn": firstname, "nameFourn": lastname})
-        .then(function (response) { output.textContent = response })
-        .catch(function(failure) { alert ('Error') })
-
-    // Prevent HTML posting form
-    event.preventDefault();
 });
