267b1b0ef087d9de3be3c75374e7136471a5bf84
Facou <facoubot2002@gmail.com>
2024-05-13 19:38:15 +0200

    Changements de ma branche mises dans le main

--- HTML-CSS/Html/StatsEtMajStocks_Fernando/MajStock.php
+++ HTML-CSS/Html/StatsEtMajStocks_Fernando/MajStock.php
@@ -1,20 +1,43 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../style.css">
+    <script src="https://code.jquery.com/jquery-latest.js"></script>
     <title>Mise à jour Stock</title>
 </head>
+
 <body>
-    <form script="accepter.php">
-        <select name="produit" title="Select a product">
-            <option value="produit1">Produit 1</option>
-            <option value="produit2">Produit 2</option>
-            <option value="produit3">Produit 3</option>
-            <option value="produit4">Produit 4</option>
-        </select>
-        
-        <input type="submit" value="Submit">
-    </form>
+    <div class="wrapper">
+        <header>
+            <h1>Mise à jour Stock<</h1>
+            <nav>
+                <a href="../">Retour</a>
+            </nav>
+        </header>
+
+        <fieldset>
+            <legend> Mise à jour Stock lors de la réception</legend>
+            <form class="formulaireIngredients" id="formulaireIngredients">
+
+                <label for="fournisseurs">Nom fournisseur:</label>
+                <select id="fournisseurs" class="fournisseurs" name="fournisseurs" title="fournisseurs" required>
+                    <option value="">Choissisez un fournisseur parmi la liste</option>
+                </select>
+
+                <div id="ingredients" class="ingredients">
+                </div>
+                
+            </form>
+        </fieldset>
+    </div>
 </body>
+
+
+<footer>
+    <script src="../../../Scripts/JavaScript/Fernando/majStock/affichageDynamiqueFournisseurs.js"></script>
+    <script src="../../../Scripts/JavaScript/Fernando/majStock/affichageDynamiqueIngredients.js"></script>
+</footer>
+
 </html>
---
+++ HTML-CSS/Html/StatsEtMajStocks_Fernando/stats&analyse.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mise à jour Stock</title>
+    <link rel="stylesheet" href="../../style.css">
+    <script src="https://code.jquery.com/jquery-latest.js"></script>
+</head>
+
+<body>
+    <label for="crepes">Produits :</label>
+
+    <select id="crepes" class="crepes" name="crepes" title="crepes" required>
+        <option value="">Choissisez un produit parmi la liste</option>
+    </select>
+</body>
+
+<footer>
+    <script src="../../../../Scripts/JavaScript/Fernando/statsAnalyse/affichageDynamiqueCrepes.js"></script>
+</footer>
+
+</html>
