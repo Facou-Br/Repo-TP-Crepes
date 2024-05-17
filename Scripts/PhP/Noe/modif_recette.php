f8141b9fa3428e8dab73d8f5e93d2f2495c3a19b
Nemou39 <97458585+Nemou39@users.noreply.github.com>
2024-05-17 13:06:56 +0200

    Modification des recettes - wip

---
+++ Scripts/PhP/Noe/modif_recette.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '../../../../BaseDeDonnees/codesConnexion.php';
+try {
+    $connex = new PDO('mysql:host=' . HOST . ';charset=utf8;dbname=' . DATABASE . ';port=' . PORT, ADMIN_USER, ADMIN_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (PDOException $e) {
+    echo 'Erreur : ' . $e->getMessage() . '<br />';
+    echo 'N° : ' . $e->getCode();
+    die();
+}
+;
+$nomFourn = $_POST["nomFourn"];
+$adresse = $_POST["adresse"];
+$cp = $_POST["codePostal"];
+$ville = $_POST["ville"];
+$tel = $_POST["telephone"];
+try {
+    $connex->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
+    $sql = "UPDATE fournisseur SET Adresse = '".$adresse."', CodePostal = '".$cp."', Ville = '".$ville."', Tel = '".$tel."' WHERE fournisseur.NomFourn = '".$nomFourn."';";
+    echo($sql);
+    $connex->exec($sql); #essayer le query pour voir si ça marche regarder dans le cours
+    $connex->commit();
+    echo "Fournisseur modifié";
+} catch (PDOException $e) {
+    echo 'Erreur : ' . $e->getMessage() . '<br />';
+    echo 'N° : ' . $e->getCode();
+    die();
+}
+
+?>
