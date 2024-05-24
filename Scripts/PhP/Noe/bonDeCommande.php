9218ea7f242b634d7b0e4574484ede4ce7111e82
Quentin <quentin.mester@gmail.com>
2024-05-24 16:53:29 +0200

    Bon de commande OK

--- Scripts/PhP/Noe/bonDeCommande.php
+++ Scripts/PhP/Noe/bonDeCommande.php
@@ -7,72 +7,132 @@
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     </head>
     <body>
-        <div>
-            <form action="generationPDF.php" metxzhod="post">
-                <label for="fournisseur">Fournisseur :</label>
-                <select id="fournisseur" name="fournisseur" required>
-                    <?php
-                        require_once '../../../BaseDeDonnees/codesConnexion.php';
+        <div class="wrapper">
+            <form action="generationPDF.php" method="post" target="_blank">
+                <div class="groupe-formulaire">
+                    <label for="fournisseur">Fournisseur :</label>
+                    <select id="fournisseur" name="fournisseur" required>
+                        <?php
+                            require_once '../../../BaseDeDonnees/codesConnexion.php';
 
-                        try {
-                            $connex = new PDO('mysql:host=' . HOST . ';charset=utf8;dbname=' . DATABASE, ADMIN_USER, ADMIN_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                        } catch (Exception $e) {
-                            echo 'Erreur : ' . $e->getMessage() . '<br/>';
-                            echo 'N° : ' . $e->getCode();
-                            die();
-                        }
+                            try {
+                                $connex = new PDO('mysql:host=' . HOST . ';charset=utf8;dbname=' . DATABASE, ADMIN_USER, ADMIN_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                            } catch (Exception $e) {
+                                echo 'Erreur : ' . $e->getMessage() . '<br/>';
+                                echo 'N° : ' . $e->getCode();
+                                die();
+                            }
 
-                        $rq = "SELECT NomFourn FROM FOURNISSEUR ORDER BY NomFourn ASC";
-                        $resultat = $connex->query($rq);
+                            $rq = "SELECT NomFourn FROM FOURNISSEUR ORDER BY NomFourn ASC";
+                            $resultat = $connex->query($rq);
 
-                        while ($ligne = $resultat->fetch(PDO::FETCH_ASSOC)) {
-                            echo "<option value='{$ligne['NomFourn']}'>{$ligne['NomFourn']}</option>";
-                        }
-                    ?>
-                </select>
+                            while ($ligne = $resultat->fetch(PDO::FETCH_ASSOC)) {
+                                echo "<option value='{$ligne['NomFourn']}'>{$ligne['NomFourn']}</option>";
+                            }
+                        ?>
+                    </select>
+                </div>
 
-                <label for="nom">Nom :</label>
-                <input type="text" id="nom" name="nom" required>
+                <div class="groupe-formulaire">
+                    <label for="nom">Nom :</label>
+                    <input type="text" id="nom" name="nom" required>
+                </div>
 
-                <label for="date">Date de livraison :</label>
-                <input type="date" id="date" name="date" required>
+                <div class="groupe-formulaire">
+                    <label for="date">Date de livraison :</label>
+                    <input type="date" id="date" name="date" required>
+                </div>
 
-                <label for="ingredients">Ingrédients :</label>
-                <select id="ingredients" name="ingredients" required>
-                    <!-- Affichage des ingrédients -->
-                </select>
-
-                <label for="quantite">Quantité :</label>
-                <input type="number" id="quantite" name="quantite" min="1">
+                <div id="conteneur-ingredients">
+                    <div class="groupe-formulaire ingredient">
+                        <label for="ingredients">Ingrédient</label>
+                        <select class="ingredients" name="ingredients[]" required>
+                            <!-- Affichage des ingrédients -->
+                        </select>
+                        <label for="quantite">Quantité</label>
+                        <input type="number" class="quantite" name="quantite[]" min="1" required>
+                        <button type="button" class="btn-ajouter-ingredient">+</button>
+                    </div>
+                </div>
 
                 <input type="submit" value="Envoyer">
             </form>
         </div>
 
+        <!-- Affichage dynamique des ingrédients -->
         <script>
             $(document).ready(function() {
-                $('#fournisseur').on('change', function() {
-                    var fournisseur = $(this).val();
+                var maxIngredients = 10;
 
+                function chargerIngredients(selectElement, fournisseur) {
                     $.ajax({
                         type: "POST",
                         url: "chargerIngredients.php",
                         data: JSON.stringify({ fournisseur: fournisseur }),
                         contentType: "application/json",
                         success: function(response) {
-                            var ingredientsSelect = $('#ingredients');
-                            ingredientsSelect.empty();
-
                             var ingredients = JSON.parse(response);
+                            selectElement.empty();
                             ingredients.forEach(function(ingredient) {
                                 var option = $('<option></option>').attr('value', ingredient.NomIngred).text(ingredient.NomIngred);
-                                ingredientsSelect.append(option);
+                                selectElement.append(option);
                             });
+                            mettreAJourOptionsIngredients();
                         },
                         error: function(error) {
                             console.error("Erreur : ", error);
                         }
                     });
+                }
+
+                function mettreAJourOptionsIngredients() {
+                    var ingredientsSelectionnes = [];
+                    $('.ingredients').each(function() {
+                        var valeurSelectionnee = $(this).val();
+                        if (valeurSelectionnee) {
+                            ingredientsSelectionnes.push(valeurSelectionnee);
+                        }
+                    });
+
+                    $('.ingredients').each(function() {
+                        var selectActuel = $(this);
+                        selectActuel.find('option').each(function() {
+                            if (ingredientsSelectionnes.includes($(this).attr('value')) && $(this).attr('value') !== selectActuel.val()) {
+                                $(this).hide();
+                            } else {
+                                $(this).show();
+                            }
+                        });
+                    });
+                }
+
+                $('#fournisseur').on('change', function() {
+                    var fournisseur = $(this).val();
+                    $('.ingredients').each(function() {
+                        chargerIngredients($(this), fournisseur);
+                    });
+                });
+
+                $(document).on('change', '.ingredients', function() {
+                    mettreAJourOptionsIngredients();
+                });
+
+                $(document).on('click', '.btn-ajouter-ingredient', function() {
+                    if ($('.ingredient').length < maxIngredients) {
+                        var nouveauGroupeIngredient = $('.ingredient:first').clone();
+                        nouveauGroupeIngredient.find('select').empty();
+                        nouveauGroupeIngredient.find('input').val('');
+                        nouveauGroupeIngredient.find('.btn-ajouter-ingredient').remove();
+                        nouveauGroupeIngredient.append('<button type="button" class="btn-supprimer-ingredient">-</button>');
+                        $('#conteneur-ingredients').append(nouveauGroupeIngredient);
+                        var fournisseur = $('#fournisseur').val();
+                        chargerIngredients(nouveauGroupeIngredient.find('select'), fournisseur);
+                    }
+                });
+
+                $(document).on('click', '.btn-supprimer-ingredient', function() {
+                    $(this).closest('.ingredient').remove();
+                    mettreAJourOptionsIngredients();
                 });
             });
         </script>
--- Scripts/PhP/Noe/generationPDF.php
+++ Scripts/PhP/Noe/generationPDF.php
@@ -1,41 +1,51 @@
 <?php
     require('tfpdf.php');
 
-    $fournisseur = $_POST['fournisseur'];
-    $nom = $_POST['nom'];
-    $date = $_POST['date'];
-    $ingredient = $_POST['ingredients'];
-    $quantite = $_POST['quantite'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $fournisseur = $_POST['fournisseur'];
+        $nom = $_POST['nom'];
+        $date = $_POST['date'];
+        $ingredients = $_POST['ingredients'];
+        $quantites = $_POST['quantite'];
 
-    class PDF extends tFPDF {
-        function Header() {
-            $this->SetFont('Arial', 'B', 14);
-            $this->Cell(0, 10, 'Bon de commande : ' . $_POST['fournisseur'], 0, 1, 'C');
-            $this->Ln(10);
+        class PDF extends tFPDF {
+            function Header() {
+                $this->SetFont('Arial', 'B', 14);
+                $this->Cell(0, 10, 'Bon de commande : ' . $_POST['fournisseur'], 0, 1, 'C');
+                $this->Ln(10);
+            }
+
+            function Footer() {
+                $this->SetY(-15);
+                $this->SetFont('Arial', 'I', 8);
+                $this->Cell(0, 10, 'Page ' . $this->PageNo(), 0, 0, 'C');
+            }
         }
 
-        function Footer() {
-            $this->SetY(-15);
-            $this->SetFont('Arial', 'I', 8);
-            $this->Cell(0, 10, 'Page ' . $this->PageNo(), 0, 0, 'C');
+        $pdf = new PDF();
+        $pdf->AddPage();
+
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(0, 10, 'Informations de la commande', 0, 1);
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(50, 10, 'Nom : ', 0, 0);
+        $pdf->Cell(0, 10, $nom, 0, 1);
+        $pdf->Cell(50, 10, 'Date de livraison : ', 0, 0);
+        $pdf->Cell(0, 10, $date, 0, 1);
+        $pdf->Ln(10);
+
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(0, 10, 'Détails de la commande', 0, 1);
+        $pdf->SetFont('Arial', '', 12);
+
+        for ($i = 0; $i < count($ingredients); $i++) {
+            $pdf->Cell(50, 10, 'Ingrédient : ', 0, 0);
+            $pdf->Cell(0, 10, $ingredients[$i], 0, 1);
+            $pdf->Cell(50, 10, 'Quantité : ', 0, 0);
+            $pdf->Cell(0, 10, $quantites[$i], 0, 1);
+            $pdf->Ln(5);
         }
+
+        $pdf->Output('bonDeCommande.pdf', 'D');
     }
-
-    $pdf = new PDF();
-    $pdf->AddPage();
-
-    $pdf->SetFont('Arial', 'B', 12);
-    $pdf->Cell(0, 10, 'Informations de la commande', 0, 1);
-    $pdf->SetFont('Arial', '', 12);
-    $pdf->Cell(0, 10, 'Nom : ' . $nom, 0, 1);
-    $pdf->Cell(0, 10, 'Date de livraison : ' . $date, 0, 1);
-    $pdf->Ln(10);
-
-    $pdf->SetFont('Arial', 'B', 12);
-    $pdf->Cell(0, 10, 'Détails de la commande', 0, 1);
-    $pdf->SetFont('Arial', '', 12);
-    $pdf->Cell(0, 10, 'Ingrédient : ' . $ingredient, 0, 1);
-    $pdf->Cell(0, 10, 'Quantité : ' . $quantite, 0, 1);
-
-    $pdf->Output('bonDeCommande.pdf', 'D');
 ?>
