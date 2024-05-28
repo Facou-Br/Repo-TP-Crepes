7c58c0dcc93184ecacf843d513e8c30bbd77f84a
Nemou39 <97458585+Nemou39@users.noreply.github.com>
2024-05-28 11:01:56 +0200

    Ajout recette + modifier recette (wip)

--- Scripts/JavaScript/GestionsRecettes/modif_recette.js
+++ Scripts/JavaScript/GestionsRecettes/modif_recette.js
@@ -1,29 +1,42 @@
 $.ajax({
-    url: "../../../Scripts/PhP/Owen/fournisseur/afficher_fourn.php",
+    url: "../../PhP/Noe/affich_recette.php",
     type: "POST",
     success: function (data) {
         data = JSON.parse(data);
-            $.each(data, function (index, fournisseur) {
-                $("#fournisseur-select").append(
-                    $("<option></option>").val(fournisseur["NomFourn"]).text(fournisseur["NomFourn"])
+            $.each(data, function (index, recette) {
+                $("#recette-select").append(
+                    $("<option></option>").val(recette["NomProd"]).text(recette["NomProd"])
                 );
             });
-        $("#fournisseur-select").change(function() {
-            var selectedFournisseur = $(this).val();
-            var fournisseur = data.find(f => f.NomFourn === selectedFournisseur);
+        $("#recette-select").change(function() {
+            var selectedRecette = $(this).val();
+            var recette = data.find(r => r.NomProd === selectedRecette);
 
-            if (fournisseur) {
-                $("#nom").val(fournisseur.NomFourn);
-                $("#adresse").val(fournisseur.Adresse);
-                $("#ville").val(fournisseur.Ville);
-                $("#codePostal").val(fournisseur.CodePostal);
-                $("#telephone").val(fournisseur.Tel);
+            if (recette) {
+                $("#NomProd").val(recette.NomProd);
+                $("#Taille").val(recette.Taille);
+                $("#NbIngBase").val(recette.NbIngBase);
+                $("#Image").val(recette.Image);
+                $("#PrixUHT").val(recette.PrixUHT);
+                $("#IngBase1").val(recette.IngBase1);
+                $("#IngBase2").val(recette.IngBase2);
+                $("#IngBase3").val(recette.IngBase3);
+                $("#IngBase4").val(recette.IngBase4);
+                $("#IngBase5").val(recette.IngBase5);
+                $("#IngOpt1").val(recette.IngOpt1);
+                $("#IngOpt2").val(recette.IngOpt2);
+                $("#IngOpt3").val(recette.IngOpt3);
+                $("#IngOpt4").val(recette.IngOpt4);
+                $("#IngOpt5").val(recette.IngOpt5);
+                $("#IngOpt6").val(recette.IngOpt6);
+                $("#NbOptMax").val(recette.NbOptMax);
+                $("#DateArchiv").val(recette.DateArchiv);
             }
         });
-        }, // closing brace and parenthesis for success function
+        },
 
     error: function () {
-        alert("Erreur lors de la récupération des fournisseurs.");
+        alert("Erreur lors de la récupération des recettes.");
     }
 });
 
@@ -31,33 +44,69 @@ $.ajax({
 $(document).ready(function(){
     $('#modifier').click(function(e){
         e.preventDefault();
-        var nomFourn = $('#fournisseur-select').val();
-        var adresse = $('#adresse').val();
-        var codePostal = $('#codePostal').val();
-        var ville = $('#ville').val();
-        var telephone = $('#telephone').val();
+        var NomProd = $('#recette-select').val();
+        var Taille = $('#Taille').val();
+        var NbIngBase = $('#NbIngBase').val();
+        var Image = $('#Image').val();
+        var IngBase1 = $('#IngBase1').val();
+        var IngBase2 = $('#IngBase2').val();
+        var IngBase3 = $('#IngBase3').val();
+        var IngBase4 = $('#IngBase4').val();
+        var IngBase5 = $('#IngBase5').val();
+        var IngOpt1 = $('#IngOpt1').val();
+        var IngOpt2 = $('#IngOpt2').val();
+        var IngOpt3 = $('#IngOpt3').val();
+        var IngOpt4 = $('#IngOpt4').val();
+        var IngOpt5 = $('#IngOpt5').val();
+        var IngOpt6 = $('#IngOpt6').val();
+        var NbOptMax = $('#NbOptMax').val();
+        var DateArchiv = $('#DateArchiv').val();
 
         $.ajax({
             type: 'POST',
-            url: '../../../Scripts/PHP/Owen/fournisseur/modifier_fourn.php',
+            url: '../../../Scripts/PHP/Noe/modif_recette.php',
             data: {
-                nomFourn: nomFourn,
-                adresse: adresse,
-                codePostal: codePostal,
-                ville: ville,
-                telephone: telephone
+                NomProd: NomProd,
+                Taille: Taille,
+                NbIngBase: NbIngBase,
+                Image: Image,
+                IngBase1: IngBase1,
+                IngBase2: IngBase2,
+                IngBase3: IngBase3,
+                IngBase4: IngBase4,
+                IngBase5: IngBase5,
+                IngOpt1: IngOpt1,
+                IngOpt2: IngOpt2,
+                IngOpt3: IngOpt3,
+                IngOpt4: IngOpt4,
+                IngOpt5: IngOpt5,
+                IngOpt6: IngOpt6,
+                NbOptMax: NbOptMax,
+                DateArchiv: DateArchiv,
             },
             success: function(response){
-                alert("Fournisseur modifié avec succès"); //Popup au lieu d'alerte
+                alert("Recette modifié avec succès"); //Popup au lieu d'alerte
                 $("#resultat").append(response);
-                var fournisseur = data.find(f => f.NomFourn === selectedFournisseur);
+                var recette = data.find(r => r.NomProd === selectedRecette);
 
-                if (fournisseur) {
-                    $("#nom").val("");
-                    $("#adresse").val("");
-                    $("#ville").val("");
-                    $("#codePostal").val("");
-                    $("#tel").val("");
+                if (recette) {
+                    $('#nomProd').val("");
+                    $('#Taille').val("");
+                    $('#NbIngBase').val("");
+                    $('#Image').val("");
+                    $('#IngBase1').val("");
+                    $('#IngBase2').val("");
+                    $('#IngBase3').val("");
+                    $('#IngBase4').val("");
+                    $('#IngBase5').val("");
+                    $('#IngOpt1').val("");
+                    $('#IngOpt2').val("");
+                    $('#IngOpt3').val("");
+                    $('#IngOpt4').val("");
+                    $('#IngOpt5').val("");
+                    $('#IngOpt6').val("");
+                    $('#NbOptMax').val("");
+                    $('#DateArchiv').val("");
                 }
             },
             fail: function () {
---
+++ Scripts/PhP/Noe/affich_recette.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../../../../BaseDeDonnees/codesConnexion.php';
+try {
+    $connex = new PDO('mysql:host=' . HOST . ';charset=utf8;dbname=' . DATABASE.';port='.PORT, ADMIN_USER, ADMIN_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (PDOException $e) {
+    echo 'Erreur : ' . $e->getMessage() . '<br />';
+    echo 'N° : ' . $e->getCode();
+    die();
+}
+try{
+    $connex->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
+    $requete1 = 'SELECT NomProd FROM produit;';
+    $ligne = $connex->query($requete1);
+}
+catch (PDOException $e) {
+    echo 'Erreur : ' . $e->getMessage() . '<br />';
+    echo 'N° : ' . $e->getCode();
+    die();
+}
+
+$result=array();
+foreach ($ligne as $row) {
+    $result[]=$row;
+}
+$stocksJson = json_encode($result);
+echo $stocksJson;
+?>
