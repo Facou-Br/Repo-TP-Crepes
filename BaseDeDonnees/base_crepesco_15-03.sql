37d86b50e1c27043c486f1fa05201c8f359e1bba
Facou <facoubot2002@gmail.com>
2024-05-30 10:59:43 +0200

    chore: Update database with new ingredient prices from suppliers

--- BaseDeDonnees/base_crepesco_15-03.sql
+++ BaseDeDonnees/base_crepesco_15-03.sql
@@ -87,18 +87,18 @@ INSERT INTO `COM_DET` (`Num_OF`, `Quant`, `NumCom`) VALUES
 --
 
 CREATE TABLE `DETAIL` (
-                          `Num_OF` int NOT NULL,
-                          `NomProd` char(30) NOT NULL,
-                          `IngBase1` char(20) NOT NULL,
-                          `IngBase2` char(20) DEFAULT NULL,
-                          `IngBase3` char(20) DEFAULT NULL,
-                          `IngBase4` char(20) DEFAULT NULL,
-                          `IngOpt1` char(20) DEFAULT NULL,
-                          `IngOpt2` char(20) DEFAULT NULL,
-                          `IngOpt3` char(20) DEFAULT NULL,
-                          `IngOpt4` char(20) DEFAULT NULL,
-                          `DateArchiv` date DEFAULT NULL,
-                          `IdProd` int NOT NULL
+  `Num_OF` int NOT NULL,
+  `NomProd` char(30) NOT NULL,
+  `IngBase1` char(20) NOT NULL,
+  `IngBase2` char(20) DEFAULT NULL,
+  `IngBase3` char(20) DEFAULT NULL,
+  `IngBase4` char(20) DEFAULT NULL,
+  `IngOpt1` char(20) DEFAULT NULL,
+  `IngOpt2` char(20) DEFAULT NULL,
+  `IngOpt3` char(20) DEFAULT NULL,
+  `IngOpt4` char(20) DEFAULT NULL,
+  `DateArchiv` date DEFAULT NULL,
+  `IdProd` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -106,9 +106,9 @@ CREATE TABLE `DETAIL` (
 --
 
 INSERT INTO `DETAIL` (`Num_OF`, `NomProd`, `IngBase1`, `IngBase2`, `IngBase3`, `IngBase4`, `IngOpt1`, `IngOpt2`, `IngOpt3`, `IngOpt4`, `DateArchiv`, `IdProd`) VALUES
-                                                                                                                                                                   (1, 'CrepesSuzette', 'Farine', 'Oeufs', 'Lait', 'Nutella', 'Chocolat', 'Fraise', 'Banane', 'Chantilly', '2024-03-13', 1),
-                                                                                                                                                                   (2, 'CrepesBretonnes', 'Farine', 'Oeufs', 'Lait', 'Oeuf', 'Jambom', 'Emental', 'Fraise', 'Banane', '2024-03-13', 2),
-                                                                                                                                                                   (3, 'Salade', 'Tomate', 'Salade', 'Pomme de terre', 'Carotte', 'Courgette', 'Aubergine', 'Poivron', 'Oignon', '2024-03-13', 3);
+  (1, 'CrepesSuzette', 'Farine', 'Oeufs', 'Lait', 'Nutella', 'Chocolat', 'Fraise', 'Banane', 'Chantilly', '2024-03-13', 1),
+  (2, 'CrepesBretonnes', 'Farine', 'Oeufs', 'Lait', 'Oeuf', 'Jambom', 'Emental', 'Fraise', 'Banane', '2024-03-13', 2),
+  (3, 'Salade', 'Tomate', 'Salade', 'Pomme de terre', 'Carotte', 'Courgette', 'Aubergine', 'Poivron', 'Oignon', '2024-03-13', 3);
 
 -- --------------------------------------------------------
 
@@ -117,8 +117,8 @@ INSERT INTO `DETAIL` (`Num_OF`, `NomProd`, `IngBase1`, `IngBase2`, `IngBase3`, `
 --
 
 CREATE TABLE `DET_INGR` (
-                            `Num_OF` int NOT NULL,
-                            `IdIngred` int NOT NULL
+  `Num_OF` int NOT NULL,
+  `IdIngred` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -126,9 +126,9 @@ CREATE TABLE `DET_INGR` (
 --
 
 INSERT INTO `DET_INGR` (`Num_OF`, `IdIngred`) VALUES
-                                                  (1, 1),
-                                                  (2, 2),
-                                                  (3, 3);
+  (1, 1),
+  (2, 2),
+  (3, 3);
 
 -- --------------------------------------------------------
 
@@ -137,12 +137,12 @@ INSERT INTO `DET_INGR` (`Num_OF`, `IdIngred`) VALUES
 --
 
 CREATE TABLE `FOURNISSEUR` (
-                               `NomFourn` char(25) NOT NULL,
-                               `Adresse` char(30) NOT NULL,
-                               `CodePostal` char(5) NOT NULL,
-                               `Ville` char(20) NOT NULL,
-                               `Tel` char(12) NOT NULL,
-                               `DateArchiv` date DEFAULT NULL
+  `NomFourn` char(25) NOT NULL,
+  `Adresse` char(30) NOT NULL,
+  `CodePostal` char(5) NOT NULL,
+  `Ville` char(20) NOT NULL,
+  `Tel` char(12) NOT NULL,
+  `DateArchiv` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -150,16 +150,16 @@ CREATE TABLE `FOURNISSEUR` (
 --
 
 INSERT INTO `FOURNISSEUR` (`NomFourn`, `Adresse`, `CodePostal`, `Ville`, `Tel`, `DateArchiv`) VALUES
-                                                                                                  ('Coca-Cola', '9 Chemin de Bretagne', '92130', 'ISSY LES MOULINEAUX', '01 46 88 89', '2024-03-13'),
-                                                                                                  ('Danone', '17 boulevard Haussmann', '75009', 'PARIS', '01 44 35 20', '2024-03-13'),
-                                                                                                  ('Ferrero', '18 Rue Jacques Monod CS 90058', '76136', 'MONT SAINT AIGNAN', '0800 553 553', '2024-03-13'),
-                                                                                                  ('Heineken', '4 rue de la Mare Blanche', '77186', 'NOISIEL', '01 60 37 68', '2024-03-13'),
-                                                                                                  ('Lactalis', '42 rue Rieussec', '53000', 'LAVAL', '02 43 59 59', '2024-03-13'),
-                                                                                                  ('Mozzalat', '28 rue Gay Lussac - France', '75005', 'PARIS', '02 32 39 71', '2024-03-13'),
-                                                                                                  ('Nestle', '7 boulevard Pierre Carle', '77186', 'NOISIEL', '01 60 37 68', '2024-03-13'),
-                                                                                                  ('Nutella', '18 Rue Jacques Monod CS 90058', '76136', 'MONT SAINT AIGNAN', '0800 553 553', '2024-03-13'),
-                                                                                                  ('PepsiCo', '420 rue d\'Estienne d\'Orves', '92705', 'COLOMBES', '01 41 04 11', '2024-03-13'),
-                                                                                                  ('TransGourmet', 'Lieudit Les Bonnes Filles', '21200', 'Levernois', '0826 101 710', '2024-03-13');
+  ('Coca-Cola', '9 Chemin de Bretagne', '92130', 'ISSY LES MOULINEAUX', '01 46 88 89', '2024-03-13'),
+  ('Danone', '17 boulevard Haussmann', '75009', 'PARIS', '01 44 35 20', '2024-03-13'),
+  ('Ferrero', '18 Rue Jacques Monod CS 90058', '76136', 'MONT SAINT AIGNAN', '0800 553 553', '2024-03-13'),
+  ('Heineken', '4 rue de la Mare Blanche', '77186', 'NOISIEL', '01 60 37 68', '2024-03-13'),
+  ('Lactalis', '42 rue Rieussec', '53000', 'LAVAL', '02 43 59 59', '2024-03-13'),
+  ('Mozzalat', '28 rue Gay Lussac - France', '75005', 'PARIS', '02 32 39 71', '2024-03-13'),
+  ('Nestle', '7 boulevard Pierre Carle', '77186', 'NOISIEL', '01 60 37 68', '2024-03-13'),
+  ('Nutella', '18 Rue Jacques Monod CS 90058', '76136', 'MONT SAINT AIGNAN', '0800 553 553', '2024-03-13'),
+  ('PepsiCo', '420 rue d\'Estienne d\'Orves', '92705', 'COLOMBES', '01 41 04 11', '2024-03-13'),
+  ('TransGourmet', 'Lieudit Les Bonnes Filles', '21200', 'Levernois', '0826 101 710', '2024-03-13');
 
 -- --------------------------------------------------------
 
@@ -168,9 +168,9 @@ INSERT INTO `FOURNISSEUR` (`NomFourn`, `Adresse`, `CodePostal`, `Ville`, `Tel`, 
 --
 
 CREATE TABLE `FOURN_INGR` (
-                              `NomFourn` char(25) NOT NULL,
-                              `IdIngred` int NOT NULL,
-                              `PrixUHT` float NOT NULL
+  `NomFourn` char(25) NOT NULL,
+  `IdIngred` int NOT NULL,
+  `PrixUHT` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -178,9 +178,9 @@ CREATE TABLE `FOURN_INGR` (
 --
 
 INSERT INTO `FOURN_INGR` (`NomFourn`, `IdIngred`, `PrixUHT`) VALUES
-                                                                 ('Coca-Cola', 29, 3),
-                                                                 ('Ferrero', 5, 12),
-                                                                 ('Nestle', 3, 2);
+  ('Coca-Cola', 29, 3),
+  ('Ferrero', 5, 12),
+  ('Nestle', 3, 2);
 
 -- --------------------------------------------------------
 
@@ -189,16 +189,16 @@ INSERT INTO `FOURN_INGR` (`NomFourn`, `IdIngred`, `PrixUHT`) VALUES
 --
 
 CREATE TABLE `INGREDIENT` (
-                              `IdIngred` int NOT NULL,
-                              `NomIngred` char(30) NOT NULL,
-                              `Frais` char(1) NOT NULL,
-                              `Unite` char(10) NOT NULL DEFAULT '"sans"',
-                              `SeuilStock` int NOT NULL DEFAULT '1',
-                              `StockMin` int NOT NULL,
-                              `StockReel` float NOT NULL,
-                              `PrixUHT_Moyen` float NOT NULL,
-                              `Q_A_Com` int NOT NULL,
-                              `DateArchiv` date DEFAULT NULL
+  `IdIngred` int NOT NULL,
+  `NomIngred` char(30) NOT NULL,
+  `Frais` char(1) NOT NULL,
+  `Unite` char(10) NOT NULL DEFAULT '"sans"',
+  `SeuilStock` int NOT NULL DEFAULT '1',
+  `StockMin` int NOT NULL,
+  `StockReel` float NOT NULL,
+  `PrixUHT_Moyen` float NOT NULL,
+  `Q_A_Com` int NOT NULL,
+  `DateArchiv` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -206,30 +206,30 @@ CREATE TABLE `INGREDIENT` (
 --
 
 INSERT INTO `INGREDIENT` (`IdIngred`, `NomIngred`, `Frais`, `Unite`, `SeuilStock`, `StockMin`, `StockReel`, `PrixUHT_Moyen`, `Q_A_Com`, `DateArchiv`) VALUES
-                                                                                                                                                          (1, 'Farine', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (2, 'Oeufs', '', 'unite', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (3, 'Lait', '', 'l', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (4, 'Sucre', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (5, 'Nutella', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (6, 'Chocolat', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (7, 'Fraise', '', 'g', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (8, 'Banane', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (9, 'Chantilly', '', 'l', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (10, 'Jambon', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (11, 'Bacon', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (12, 'Emmental', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (13, 'Tomate', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (14, 'Salade', '', 'unite', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (15, 'Pomme de terre', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (16, 'Carotte', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (17, 'Courgette', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (18, 'Aubergine', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (19, 'Poivron', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (20, 'Oignon', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (21, 'Ail', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (22, 'Huile', '', 'l', 1, 0, 5, 2, 1, '2024-03-13'),
-                                                                                                                                                          (23, 'Reblochon', '', 'kg', 0, 0, 0, 12, 2, '2024-03-13'),
-                                                                                                                                                          (29, 'Coca-Cola', '1', 'l', 1, 0, 2, 3, 0, '2024-03-13');
+  (1, 'Farine', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (2, 'Oeufs', '', 'unite', 1, 0, 5, 2, 1, '2024-03-13'),
+  (3, 'Lait', '', 'l', 1, 0, 5, 2, 1, '2024-03-13'),
+  (4, 'Sucre', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (5, 'Nutella', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (6, 'Chocolat', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (7, 'Fraise', '', 'g', 1, 0, 5, 2, 1, '2024-03-13'),
+  (8, 'Banane', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (9, 'Chantilly', '', 'l', 1, 0, 5, 2, 1, '2024-03-13'),
+  (10, 'Jambon', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (11, 'Bacon', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (12, 'Emmental', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (13, 'Tomate', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (14, 'Salade', '', 'unite', 1, 0, 5, 2, 1, '2024-03-13'),
+  (15, 'Pomme de terre', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (16, 'Carotte', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (17, 'Courgette', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (18, 'Aubergine', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (19, 'Poivron', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (20, 'Oignon', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (21, 'Ail', '', 'kg', 1, 0, 5, 2, 1, '2024-03-13'),
+  (22, 'Huile', '', 'l', 1, 0, 5, 2, 1, '2024-03-13'),
+  (23, 'Reblochon', '', 'kg', 0, 0, 0, 12, 2, '2024-03-13'),
+  (29, 'Coca-Cola', '1', 'l', 1, 0, 2, 3, 0, '2024-03-13');
 
 -- --------------------------------------------------------
 
@@ -238,13 +238,13 @@ INSERT INTO `INGREDIENT` (`IdIngred`, `NomIngred`, `Frais`, `Unite`, `SeuilStock
 --
 
 CREATE TABLE `LIVREUR` (
-                           `IdLivreur` int NOT NULL,
-                           `Nom` char(20) NOT NULL,
-                           `Prenom` char(20) NOT NULL,
-                           `Tel` char(16) NOT NULL,
-                           `NumSS` char(15) NOT NULL,
-                           `Disponible` char(1) NOT NULL,
-                           `DateArchiv` date DEFAULT NULL
+  `IdLivreur` int NOT NULL,
+  `Nom` char(20) NOT NULL,
+  `Prenom` char(20) NOT NULL,
+  `Tel` char(16) NOT NULL,
+  `NumSS` char(15) NOT NULL,
+  `Disponible` char(1) NOT NULL,
+  `DateArchiv` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -252,16 +252,16 @@ CREATE TABLE `LIVREUR` (
 --
 
 INSERT INTO `LIVREUR` (`IdLivreur`, `Nom`, `Prenom`, `Tel`, `NumSS`, `Disponible`, `DateArchiv`) VALUES
-                                                                                                     (1, 'ROULLET', 'Remi', '01 02 03 04 06', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (2, 'DUPONT', 'Jean', '01 02 03 04 07', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (3, 'DURAND', 'Pierre', '01 02 03 04 08', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (4, 'MARTIN', 'Paul', '01 02 03 04 09', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (5, 'PETIT', 'Jacques', '01 02 03 04 10', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (6, 'LEROY', 'Alain', '01 02 03 04 11', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (7, 'MOREAU', 'René', '01 02 03 04 12', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (8, 'SIMON', 'Jean-Pierre', '01 02 03 04 13', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (9, 'LAURENT', 'Michel', '01 02 03 04 14', '1 04 07 71 014', '1', '2024-03-13'),
-                                                                                                     (10, 'LACROIX', 'Philippe', '01 02 03 04 15', '1 04 07 71 014', '1', '2024-03-13');
+  (1, 'ROULLET', 'Remi', '01 02 03 04 06', '1 04 07 71 014', '1', '2024-03-13'),
+  (2, 'DUPONT', 'Jean', '01 02 03 04 07', '1 04 07 71 014', '1', '2024-03-13'),
+  (3, 'DURAND', 'Pierre', '01 02 03 04 08', '1 04 07 71 014', '1', '2024-03-13'),
+  (4, 'MARTIN', 'Paul', '01 02 03 04 09', '1 04 07 71 014', '1', '2024-03-13'),
+  (5, 'PETIT', 'Jacques', '01 02 03 04 10', '1 04 07 71 014', '1', '2024-03-13'),
+  (6, 'LEROY', 'Alain', '01 02 03 04 11', '1 04 07 71 014', '1', '2024-03-13'),
+  (7, 'MOREAU', 'René', '01 02 03 04 12', '1 04 07 71 014', '1', '2024-03-13'),
+  (8, 'SIMON', 'Jean-Pierre', '01 02 03 04 13', '1 04 07 71 014', '1', '2024-03-13'),
+  (9, 'LAURENT', 'Michel', '01 02 03 04 14', '1 04 07 71 014', '1', '2024-03-13'),
+  (10, 'LACROIX', 'Philippe', '01 02 03 04 15', '1 04 07 71 014', '1', '2024-03-13');
 
 -- --------------------------------------------------------
 
