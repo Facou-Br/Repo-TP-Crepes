4370a72e73e28da3349ab6a5b7e0baff6449b148
R-remi <remr04@gmail.com>
2024-06-01 23:21:41 +0200

    minor changes (g oublier ce que j'ai fait dessus cet aprem)

--- Scripts/PhP/Remi/panier.php
+++ Scripts/PhP/Remi/panier.php
@@ -1,13 +1,33 @@
 <?php
 session_start();
 
-// Afficher le contenu de la session
-var_dump($_SESSION);
+// Vérifiez si la session est valide et contient les données du panier
+if (isset($_SESSION['cart'])) {
+    // Affichez le contenu de la session
+    echo '<h2>Votre panier :</h2><ul>';
+    foreach ($_SESSION['cart'] as $id => $quantity) {
+        if ($quantity > 0){
+            echo "<li>$id x $quantity</li>";
+        }
+    }
+    echo '</ul>';
+} else {
+    echo 'Votre panier est vide.';
+}
 
-// Afficher les données de session
-foreach ($_SESSION as $id => $value) {
-    echo "<p>ID: " . $id . ", Value: " . $value . "</p>";
+function annulerCommande() {
+    $_SESSION['cart'] = array();
+    header('Location: ../../../HTML-CSS/index.html');
+    exit();
+}
+
+if (isset($_POST['annulerCommande'])) {
+    annulerCommande();
 }
 ?>
 
 <button type="button" onclick="location.href='validation.php'">Valider</button>
+<form method="post">
+    <button type="submit" name="annulerCommande">Annuler la commande</button>
+</form>
+<img src="../../../HTML-CSS/Images/jpeg/breton.jpg">
