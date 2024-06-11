e46e1bde48686e369d7905fe118016c81584d532
0wen2886 <128720196+0wn2886@users.noreply.github.com>
2024-06-11 10:51:30 +0200

    oo

--- Scripts/PhP/Owen/fournisseur/modifier_fourn.php
+++ Scripts/PhP/Owen/fournisseur/modifier_fourn.php
@@ -1,19 +1,27 @@
 <?php
+// Inclut le fichier contenant les codes de connexion à la base de données
 require_once '..\..\..\..\BaseDeDonnees\codesConnexion.php';
+
+// Connecte à la base de données en utilisant les informations de connexion pour l'utilisateur 'admin'
 $connex = BaseDeDonnees::connecterBDD('admin');
 
+// Récupère les données envoyées par la méthode POST
 $nomFourn = $_POST["nomFourn"];
 $adresse = $_POST["adresse"];
 $cp = $_POST["codePostal"];
 $ville = $_POST["ville"];
 $tel = $_POST["telephone"];
+
 try {
     $connex->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
+
+    // Requête SQL pour mettre à jour les informations du fournisseur dans la table 'fournisseur'
     $sql = "UPDATE `fournisseur` SET `Adresse` = '" . $adresse . "', `CodePostal` = '" . $cp . "', `Ville` = '" . $ville . "', `Tel` = '" . $tel . "' WHERE `fournisseur`.`NomFourn` = '" . $nomFourn . "';";
-    $connex->exec($sql); #essayer le query pour voir si ça marche regarder dans le cours
+    $connex->exec($sql);
     $connex->commit();
 } catch (PDOException $e) {
     echo 'Erreur : ' . $e->getMessage() . '<br />';
     echo 'N° : ' . $e->getCode();
     die();
 }
+?>
--- Scripts/PhP/Owen/fournisseur/supprimer_fourn.php
+++ Scripts/PhP/Owen/fournisseur/supprimer_fourn.php
@@ -1,16 +1,24 @@
 <?php
+// Inclut le fichier contenant les codes de connexion à la base de données
 require_once '..\..\..\..\BaseDeDonnees\codesConnexion.php';
+
+// Connecte à la base de données en utilisant les informations de connexion pour l'utilisateur 'admin'
 $connex = BaseDeDonnees::connecterBDD('admin');
 
 try {
+    // Récupère le nom du fournisseur envoyé par la méthode POST
     $nomFourn = $_POST["nomFourn"];
+
     $connex->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
+
+    // Requête SQL pour mettre à jour la date d'archivage du fournisseur dans la table 'fournisseur'
     $sql = "UPDATE `fournisseur` SET `DateArchiv`=CAST(NOW() AS DATE) WHERE `NomFourn` = '" . $nomFourn . "';";
 
-    $connex->exec($sql); #essayer le query pour voir si ça marche regarder dans le cours
+    $connex->exec($sql);
     $connex->commit();
 } catch (PDOException $e) {
     echo 'Erreur : ' . $e->getMessage() . '<br />';
     echo 'N° : ' . $e->getCode();
     die();
 }
+?>
