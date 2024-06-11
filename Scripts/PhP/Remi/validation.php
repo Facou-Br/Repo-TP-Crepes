d408e96048a6e1cf84758cf660ba5c9157a99377
R-remi <remr04@gmail.com>
2024-06-11 14:07:27 +0200

    normalement la base est fini

--- Scripts/PhP/Remi/validation.php
+++ Scripts/PhP/Remi/validation.php
@@ -12,73 +12,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $coutLiv = 5.00;
     $total = floatval($_POST['total']);
     $totalTTC = $total + $coutLiv;
-
     require_once '..\..\..\BaseDeDonnees\codesConnexion.php';
     $pdo = BaseDeDonnees::connecterBDD('admin');
-
     try {
+
+
         $requete1 = "INSERT INTO commande (NomClient, TelClient, AdrClient, CP_Client, VilClient, Date, HeureDispo, TypeEmbal, A_Livrer, EtatCde, EtatLivraison, CoutLiv, TotalTTC, IdLivreur)
                             VALUES ('$nom', '$tel', '$adresse', '$cp', '$ville', '$date_actuelle', '$heure_actuelle', 'Sac', 1, 'Acceptée', 'preparation', $coutLiv, $totalTTC, NULL);";
-        $pdo->exec($requete1);
+        $pdo->exec($requete1);      //creation de la commande
 
-        echo "<br>";
+
         $requete2 = "SELECT NumCom FROM commande WHERE NomClient = '$nom' AND TelClient = '$tel' AND AdrClient = '$adresse' AND CP_Client = '$cp' AND VilClient = '$ville' AND Date = '$date_actuelle' AND  HeureDispo = '$heure_actuelle' AND TypeEmbal = 'Sac' AND A_Livrer = 1 AND EtatCde = 'Acceptée' AND EtatLivraison = 'preparation' AND CoutLiv = $coutLiv AND TotalTTC = $totalTTC;";
-        $result = $pdo->query($requete2);
+        $result = $pdo->query($requete2);       // on recup le num de commande pour le requete 7
         $row = $result->fetch(PDO::FETCH_ASSOC);
-        $numCom = $row['NumCom']; // on recup le num de commande pour le requete TODO MODIF LE CHIFFRE
-        echo "numCom : " . $numCom;
-        echo "<br>";
+        $numCom = $row['NumCom'];
 
-        var_dump($_SESSION['cart']);
         if (isset($_SESSION['cart'])) {
             foreach ($_SESSION['cart'] as $crepe => $quantity) {
 
+
                 if ($quantity > 0) {
                     $requete3 = "SELECT IdProd FROM produit where NomProd ='$crepe';";
-                    $result = $pdo->query($requete3);
+                    $result = $pdo->query($requete3);              // on recup le num de la derniere commande pour la requete 5
                     $row = $result->fetch(PDO::FETCH_ASSOC);
-                    $IdProd = $row['IdProd'] + 1; // on recup le num de la derniere commande pour la requete TODO MODIF LE CHIFFRE
-                    echo "IdProd : " . $row['IdProd'];
-                    echo "<br>";
-                    echo "crepe : " . $crepe;
-                    echo "<br>";
+                    $IdProd = $row['IdProd'];
 
-                    $requete35 = "SELECT IngBase1, IngBase2, IngBase3, IngBase4 from produit where NomProd = '$crepe';";
-                    $result = $pdo->query($requete35);
+
+                    $requete4 = "SELECT IngBase1, IngBase2, IngBase3, IngBase4 from produit where NomProd = '$crepe';";
+                    $result = $pdo->query($requete4);       // on recup les ingredients de la crepe
                     $row = $result->fetch(PDO::FETCH_ASSOC);
                     $ingBase1 = $row['IngBase1'];
                     $ingBase2 = $row['IngBase2'];
                     $ingBase3 = $row['IngBase3'];
                     $ingBase4 = $row['IngBase4'];
-                    $requete4 = "INSERT INTO `detail` (`NomProd`, `IngBase1`, `IngBase2`, `IngBase3`, `IngBase4`, `IngOpt1`, `IngOpt2`, `IngOpt3`, `IngOpt4`, `DateArchiv`, `IdProd`)
+
+
+                    $requete5 = "INSERT INTO `detail` (`NomProd`, `IngBase1`, `IngBase2`, `IngBase3`, `IngBase4`, `IngOpt1`, `IngOpt2`, `IngOpt3`, `IngOpt4`, `DateArchiv`, `IdProd`)
                      VALUES ('$crepe', '$ingBase1', '$ingBase2', '$ingBase3', '$ingBase4', NULL, NULL, NULL, NULL, NULL, $IdProd);";
+                    $pdo->exec($requete5);              //creation de la crepe dans le detqil des commandes
 
-                    $pdo->exec($requete4);
-                    echo $requete4;
-                    echo "<br>";
 
-                    $requete5 = "SELECT MAX(Num_OF) FROM detail;";
-                    $requete5 = "SELECT MAX(Num_OF) AS max_num_of FROM detail;";
-                    $result = $pdo->query($requete5);
+                    $requete6 = "SELECT MAX(Num_OF) AS max_num_of FROM detail;";
+                    $result = $pdo->query($requete6);           // on recup le num de la derniere commande (celle de la requete5) pour la requete 7
                     $row = $result->fetch(PDO::FETCH_ASSOC);
-                    $Num_OF = $row['max_num_of']; // 654687
-                    echo "Num_OF : " . $Num_OF;
-                    echo "<br>";
+                    $Num_OF = $row['max_num_of'];
 
 
-                    $requete6 = "INSERT INTO com_det (Num_OF, Quant, NumCom) VALUES (:numOF, :quantity, :numCom); ";
-                    $stmt = $pdo->prepare($requete6);
-                    $stmt->execute(['numOF' => $Num_OF, 'quantity' => $quantity, 'numCom' => $numCom]);
-
+                    $requete7 = "INSERT INTO com_det (Num_OF, Quant, NumCom) VALUES ($Num_OF, $quantity, $numCom); ";
+                    $pdo->exec($requete5);              // associer la crepe a la commande
                 }
             }
-
         }
-
-    } catch(PDOException $e) {
-        /* header("Location: ../../../HTML-CSS/Html/Commande_Remi/index.html");*/
-
-        die("ERROR: Could not able to execute the query. " . $e->getMessage());
-
-    }}
-/*header("Location: ../../../HTML-CSS/Html/Commande_Remi/index.html");*/
+    } catch (PDOException $e) {
+        echo "<script>alert('Your order has been taken into account.'); window.location.href='../../../HTML-CSS/Html/Commande_Remi/index.html';</script>";
+        exit();
+    }
+    echo "<script>alert('Your order has been taken into account.'); window.location.href='../../../HTML-CSS/Html/Commande_Remi/index.html';</script>";
+    exit();
+}
