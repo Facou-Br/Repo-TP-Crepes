b593eae0ab9e2ce6d93714d4996f9755627b58f5
Nemou39 <97458585+Nemou39@users.noreply.github.com>
2024-06-11 11:42:06 +0200

    Alors peut-etre

--- Scripts/PhP/Noe/ajoutRecette.php
+++ Scripts/PhP/Noe/ajoutRecette.php
@@ -4,14 +4,6 @@
 <body>
 <?php
      require_once '..\..\..\BaseDeDonnees\codesConnexion.php';
-     #try {
-     #    $connex = new PDO('mysql:host=' . HOST . ';charset=utf8;dbname='
-     #        . DATABASE . ';port=' . PORT, ADMIN_USER, ADMIN_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-     #} catch (PDOException $e) {
-     #    echo 'Erreur : ' . $e->getMessage() . '<br />';
-     #    echo 'N° : ' . $e->getCode();
-     #    die();
-   # }
     $connex = BaseDeDonnees::connecterBDD('admin');
     
 
